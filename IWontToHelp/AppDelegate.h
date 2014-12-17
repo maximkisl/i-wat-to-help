e98ac35f7561429b5f2d48cf99cffbe16dd0f406
HastovIgor <mahmood1@Mahmood1s-Mac.local>
2014-12-17 15:15:36 +0100

    Initial Commit

---
+++ IWontToHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IWontToHelp
+//
+//  Created by Mac on 12/17/14.
+//  Copyright (c) 2014 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IWontToHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IWontToHelp
+//
+//  Created by Mac on 12/17/14.
+//  Copyright (c) 2014 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
